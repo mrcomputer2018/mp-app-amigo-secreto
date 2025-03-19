418d8a586fc87469ffb2845148d1a27bbad001da
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-03-19 09:57:16 -0300

    Add participant update functionality and separator to NewGroupForm component

--- app/_components/commons/NewGroupForm.tsx
+++ app/_components/commons/NewGroupForm.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import { useState } from "react";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card"
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { Mail, Trash } from "lucide-react";
 import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
 
 type LoggedUserProps = {
     id: string;
@@ -28,8 +29,20 @@ export default function NewGroupForm({loggedUser}: {loggedUser: LoggedUserProps}
 
     const [groupName, setGroupName] = useState<string>('');
 
+    
+
     function handleAddParticipant(name: string, email: string) {
         setParticipants([...participants, {name: name, email: email}]);
+    }
+
+    function updateParticipant(index: number, key: string, value: string) {
+        const newParticipants = [...participants];
+
+        if (key === "name" || key === "email") {
+            newParticipants[index][key as keyof IParticipant] = value;
+        }
+
+        setParticipants(newParticipants);
     }
 
     return (
@@ -55,40 +68,50 @@ export default function NewGroupForm({loggedUser}: {loggedUser: LoggedUserProps}
                         />
                     </div>
 
-                    <div className="space-y-2 mt-6">
+                    <div className="space-y-2 mt-8">
                         <Label htmlFor="participants" className="text-xl">
                             Participantes
                         </Label>
                         {participants.map((participant, index) => (
-                            <div key={index} className="flex space-x-2">
+                            <div key={index} className="flex flex-col md:flex-row space-x-2 md:space-x-4 space-y-2">
                                 <Input 
-                                    id={`participant-name-${index}`}
-                                    name={`participant-name-${index}`}
+                                    id={`name-${index}`}
+                                    name={`name-${index}`}
                                     type="text" 
                                     placeholder="Nome" 
                                     value={participant.name} 
                                     onChange={(e) => {
-                                        const newParticipants = [...participants];
-                                        newParticipants[index].name = e.target.value;
-                                        setParticipants(newParticipants);
-                                    }} 
+                                        updateParticipant(index,"name", e.target.value);
+                                    }}
+                                    required 
                                 />
                                 <Input 
-                                    id={`participant-email-${index}`}
-                                    name={`participant-email-${index}`}
+                                    id={`email-${index}`}
+                                    name={`email-${index}`}
                                     type="email" 
                                     placeholder="E-mail" 
+                                    className="readonly:text-muted-foreground"
+                                    readOnly={participant.email === loggedUser.email}
                                     value={participant.email} 
                                     onChange={(e) => {
                                         const newParticipants = [...participants];
                                         newParticipants[index].email = e.target.value;
                                         setParticipants(newParticipants);
                                     }} 
+                                    required
                                 />
 
+                                { participant.email === loggedUser.email ? 
+                                <Button
+                                    className="border-none hover:border-none !hover:bg-background min-w-9"
+                                    variant='outline'
+                                >
+                                </Button>
+                                : 
                                 <Button
                                     type="button"
-                                    variant='outline' 
+                                    variant='outline'
+                                    className="min-w-9" 
                                     onClick={() => {
                                         const newParticipants = [...participants];
                                         newParticipants.splice(index, 1);
@@ -97,20 +120,29 @@ export default function NewGroupForm({loggedUser}: {loggedUser: LoggedUserProps}
                                 >             
                                     <Trash size={18}/>
                                 </Button>
+                                }
+
                             </div>
                         ))}
                     </div>
-
-                    <div className="flex justify-between">
-                        <Button onClick={() => handleAddParticipant('', '')}>
+                    <Separator className="my-4" />
+                    <CardFooter className="flex flex-col md:flex-row justify-between space-y-4 md:space-y-0">
+                        <Button 
+                        type='button' 
+                        className="w-full md:w-auto"
+                        onClick={() => handleAddParticipant('', '')}
+                        >
                             Adicionar Amigo
                         </Button>
 
-                        <Button onClick={() =>{}} className="bg-destructive text-white">
+                        <Button 
+                        type='submit' 
+                        onClick={() =>{}} 
+                        className="bg-destructive text-white w-full md:w-auto flex items-center">
                             <Mail size={18} className="mr-2"/>
                             Criar grupo e enviar e-mails
                         </Button>
-                    </div>
+                    </CardFooter>
                 </form>
             </CardContent>
         </Card>
